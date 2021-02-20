57bd820ed525d67524459d58389e9d068174cf06
Yhefferson <ycicery0@misena.edu.co>
2021-02-19 23:37:07 -0500

    Ajuste conexión

---
+++ TallerMecanico/TallerMecanico.Datos/DAOConexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerMecanico.Datos
+{
+   public static class DAOConexion
+    {
+        private static string _cadenaConexion;
+
+        public static string CadenaConexion
+        {
+            get
+            {
+                if (_cadenaConexion == null)
+                {
+                    _cadenaConexion = ConfigurationManager.
+                        ConnectionStrings["Conex"].ConnectionString;
+                }
+                return _cadenaConexion;
+            }
+            set { _cadenaConexion = value; }
+        }
+    }
+}
--- TallerMecanico/TallerMecanico/Properties/Settings.Designer.cs
+++ TallerMecanico/TallerMecanico/Properties/Settings.Designer.cs
@@ -26,6 +26,7 @@ namespace TallerMecanico.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=CICERY;Initial Catalog=E_Vote;User ID=sa;Password=sa")]
         public string Conexion {
             get {
                 return ((string)(this["Conexion"]));
