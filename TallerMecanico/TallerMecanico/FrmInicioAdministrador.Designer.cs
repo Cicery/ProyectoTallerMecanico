a0d90f0777e932608cf7af96953e106d946b10fb
Hogar <Hogar@DESKTOP-IQ2LCE0>
2021-02-22 21:11:15 -0500

    Creacion Formularios: Administrador, Asistente y Administrador

--- TallerMecanico/TallerMecanico/FrmInicioAdministrador.Designer.cs
+++ TallerMecanico/TallerMecanico/FrmInicioAdministrador.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCerrarAdmin = new System.Windows.Forms.Button();
+            this.cbAdministrador = new System.Windows.Forms.ComboBox();
+            this.btnIRAdmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 22F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(219, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "USUARIO ADMINISTRADOR";
+            // 
+            // btnCerrarAdmin
+            // 
+            this.btnCerrarAdmin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrarAdmin.Location = new System.Drawing.Point(685, 12);
+            this.btnCerrarAdmin.Name = "btnCerrarAdmin";
+            this.btnCerrarAdmin.Size = new System.Drawing.Size(103, 23);
+            this.btnCerrarAdmin.TabIndex = 6;
+            this.btnCerrarAdmin.Text = "Cerrar Sesion";
+            this.btnCerrarAdmin.UseVisualStyleBackColor = true;
+            this.btnCerrarAdmin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbAdministrador
+            // 
+            this.cbAdministrador.FormattingEnabled = true;
+            this.cbAdministrador.Items.AddRange(new object[] {
+            "Modulo Administrador",
+            "Modulo Inventario",
+            "Modulo Reportes"});
+            this.cbAdministrador.Location = new System.Drawing.Point(226, 134);
+            this.cbAdministrador.Name = "cbAdministrador";
+            this.cbAdministrador.Size = new System.Drawing.Size(366, 21);
+            this.cbAdministrador.TabIndex = 7;
+            // 
+            // btnIRAdmin
+            // 
+            this.btnIRAdmin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIRAdmin.Location = new System.Drawing.Point(372, 193);
+            this.btnIRAdmin.Name = "btnIRAdmin";
+            this.btnIRAdmin.Size = new System.Drawing.Size(75, 23);
+            this.btnIRAdmin.TabIndex = 8;
+            this.btnIRAdmin.Text = "IR";
+            this.btnIRAdmin.UseVisualStyleBackColor = true;
+            this.btnIRAdmin.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FrmInicioAdministrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIRAdmin);
+            this.Controls.Add(this.cbAdministrador);
+            this.Controls.Add(this.btnCerrarAdmin);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmInicioAdministrador";
             this.Text = "FrmInicioAdministrador";
+            this.Load += new System.EventHandler(this.FrmInicioAdministrador_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCerrarAdmin;
+        private System.Windows.Forms.ComboBox cbAdministrador;
+        private System.Windows.Forms.Button btnIRAdmin;
     }
 }
--- TallerMecanico/TallerMecanico/FrmInicioAsistente.Designer.cs
+++ TallerMecanico/TallerMecanico/FrmInicioAsistente.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace TallerMecanico
+﻿
+namespace TallerMecanico
 {
     partial class FrmInicioAsistente
     {
@@ -28,12 +29,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnIRAsistente = new System.Windows.Forms.Button();
+            this.cbAsistente = new System.Windows.Forms.ComboBox();
+            this.btnCerrarAsis = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIRAsistente
+            // 
+            this.btnIRAsistente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIRAsistente.Location = new System.Drawing.Point(372, 193);
+            this.btnIRAsistente.Name = "btnIRAsistente";
+            this.btnIRAsistente.Size = new System.Drawing.Size(75, 23);
+            this.btnIRAsistente.TabIndex = 12;
+            this.btnIRAsistente.Text = "IR";
+            this.btnIRAsistente.UseVisualStyleBackColor = true;
+            // 
+            // cbAsistente
+            // 
+            this.cbAsistente.FormattingEnabled = true;
+            this.cbAsistente.Items.AddRange(new object[] {
+            "Modulo Inventario",
+            "Modulo Mantenimiento",
+            "Generar Reportes"});
+            this.cbAsistente.Location = new System.Drawing.Point(226, 134);
+            this.cbAsistente.Name = "cbAsistente";
+            this.cbAsistente.Size = new System.Drawing.Size(366, 21);
+            this.cbAsistente.TabIndex = 11;
+            // 
+            // btnCerrarAsis
+            // 
+            this.btnCerrarAsis.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrarAsis.Location = new System.Drawing.Point(685, 12);
+            this.btnCerrarAsis.Name = "btnCerrarAsis";
+            this.btnCerrarAsis.Size = new System.Drawing.Size(103, 23);
+            this.btnCerrarAsis.TabIndex = 10;
+            this.btnCerrarAsis.Text = "Cerrar Sesion";
+            this.btnCerrarAsis.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 22F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(266, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 37);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "USUARIO ASISTENTE";
+            // 
+            // FrmInicioAsistente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIRAsistente);
+            this.Controls.Add(this.cbAsistente);
+            this.Controls.Add(this.btnCerrarAsis);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmInicioAsistente";
             this.Text = "FrmInicioAsistente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnIRAsistente;
+        private System.Windows.Forms.ComboBox cbAsistente;
+        private System.Windows.Forms.Button btnCerrarAsis;
+        private System.Windows.Forms.Label label1;
     }
 }
